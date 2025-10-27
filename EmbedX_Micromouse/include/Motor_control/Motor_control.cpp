dfd65dbfdbd98eb6a3f1e7e8e544dcda182a5520
dhoangnguyen051107-jpg <dhoangnguyen051107@gmail.com>
2025-10-27 23:37:54 +0700

    Update Motor_control.cpp

--- EmbedX_Micromouse/include/Motor_control/Motor_control.cpp
+++ EmbedX_Micromouse/include/Motor_control/Motor_control.cpp
@@ -1,1 +1,67 @@
+#include "Motor_control.h"
+// Định nghĩa chân động cơ
+// Khai báo chân encoder (tiến độ hiện tại chưa dùng tới)
+const int encoderTraiPin = 34;
+const int encoderPhaiPin = 35;
+
+// Chân động cơ trái
+const int motorPWM_L = 25;
+const int motorDir1_L = 26;
+const int motorDir2_L = 27;
+
+// Chân động cơ phải
+const int motorPWM_R = 14;
+const int motorDir1_R = 12;
+const int motorDir2_R = 13;
+
+void setupMotor_control() {
+  pinMode(motorPWM_L, OUTPUT);
+  pinMode(motorDir1_L, OUTPUT);
+  pinMode(motorDir2_L, OUTPUT);
+
+  pinMode(motorPWM_R, OUTPUT);
+  pinMode(motorDir1_R, OUTPUT);
+  pinMode(motorDir2_R, OUTPUT);
+
+  ledcSetup(0, 5000, 8);
+  ledcAttachPin(motorPWM_L, 0);
+
+  ledcSetup(1, 5000, 8);
+  ledcAttachPin(motorPWM_R, 1);
+}
+
+void di_thang(int toc_do) {
+  digitalWrite(motorDir1_L, HIGH);
+  digitalWrite(motorDir2_L, LOW);
+  ledcWrite(0, toc_do);
+
+  digitalWrite(motorDir1_R, HIGH);
+  digitalWrite(motorDir2_R, LOW);
+  ledcWrite(1, toc_do);
+}
+
+void re_trai(int toc_do) {
+  digitalWrite(motorDir1_L, LOW);
+  digitalWrite(motorDir2_L, HIGH);
+  ledcWrite(0, toc_do);
+
+  digitalWrite(motorDir1_R, HIGH);
+  digitalWrite(motorDir2_R, LOW);
+  ledcWrite(1, toc_do);
+}
+
+void re_phai(int toc_do) {
+  digitalWrite(motorDir1_L, HIGH);
+  digitalWrite(motorDir2_L, LOW);
+  ledcWrite(0, toc_do);
+
+  digitalWrite(motorDir1_R, LOW);
+  digitalWrite(motorDir2_R, HIGH);
+  ledcWrite(1, toc_do);
+}
+
+void dung_lai() {
+  ledcWrite(0, 0);
+  ledcWrite(1, 0);
+}
 
