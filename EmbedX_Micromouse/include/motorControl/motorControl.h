f4aa30acd6c2848a58854145275336f5741d1753
dhoangnguyen051107-jpg <dhoangnguyen051107@gmail.com>
2025-11-21 16:55:45 +0700

    Update motorControl.h

--- EmbedX_Micromouse/include/motorControl/motorControl.h
+++ EmbedX_Micromouse/include/motorControl/motorControl.h
@@ -1,7 +1,7 @@
 #ifndef Motor_control  
 #define Motor_control
 #include <Arduino.h>
-
+#include <PID_v1.h>
 // Khai báo chân encoder
 extern const int encoder_1A;
 extern const int encoder_1B;
@@ -24,10 +24,19 @@ extern const int motorPWM_R;
 extern const int motorDir1_R;
 extern const int motorDir2_R;
 // Thông số cơ học
-//extern double duong_kinh_banh_xe = 0.065; // mét
-//extern int so_xung_encoder = 11;
-//extern double ti_le_banh_rang = 1.0;
-//extern double khoang_cach_2_banh = 0.10; // mét (chưa có thông số chính xác)
+
+// Khai báo thông số động cơ và tính toán xung di chuyển 
+extern float duong_kinh_banh_xe;//mét
+extern float kc ; // khoảng cách cần di chuyển để đi từ ô này sang ô kia là 0.18 mét
+extern float re_trai_phai; // khoảng cách mỗi bánh phải đi để rẽ đủ 90 độ (số liệu đo đạc thực nghiệm)
+extern float so_xung_1_vong_banh_xe ; // số xung xuất ra khi bánh quay đủ 1 vòng (số liệu tương đối từ đo đạc thực nghiệm)
+
+extern float so_vong_di_thang; // số vòng bánh xe để xe tiến được 1 ô
+extern float so_vong_re_trai_phai; // số vòng bánh xe để xe rẽ được 90 độ
+
+extern float xung_can_di_thang; // số xung cần truyền cho encoder biết thời điểm dừng khi đủ khoảng cách mong muốn
+extern float xung_can_re_trai_phai; // xung cần truyền cho encoder biết thời điểm rẽ đủ 90 độ
+
 // Các hàm điều khiển động cơ sẽ được gọi ở thuật toán 
 void setupMotor_control();
 void re_trai();
