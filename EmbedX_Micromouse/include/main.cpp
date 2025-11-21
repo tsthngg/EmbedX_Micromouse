7286c663dec21de8e745549cac1a21341ef527f9
nghiamatmat <doan06561@gmail.com>
2025-11-21 18:21:48 +0700

    Update main.cpp

--- EmbedX_Micromouse/include/main.cpp
+++ EmbedX_Micromouse/include/main.cpp
@@ -4,21 +4,21 @@ PID::PID(float p, float i, float d, float deltaTime, float maxI, float maxOut)
     : Kp(p), Ki(i), Kd(d), dt(deltaTime), maxIntegral(maxI), maxOutput(maxOut) {}
 
 // Tính toán giá trị PID
-float PID::tinhtoan(float setpoint, float measured) {        //measured là giá trị đo được trong thực tế , setpoint là giá trị mong muốn
+float PID::tinhtoan(float setpoint, float measured) {        //measured là giá trị đo được trong thực tế, setpoint là giá trị mong muốn
     float error = setpoint - measured;                     
-    float newIntegral = integral + error * dt;            // Integral là tích phân , để cộng dồn sai số theo thời gian để giảm thiểu sai số về lâu dài 
-    newIntegral = std::clamp(newIntegral, -maxIntegral, maxIntegral);
+    float newIntegral = integral + error * dt;            // Integral là tích phân, để cộng dồn sai số theo thời gian để giảm thiểu sai số về lâu dài 
+    newIntegral = std::clamp(newIntegral, -maxIntegral, maxIntegral);    // Tránh cộng dồn tích phân quá lớn so với sai số mà motor bù được
 
-    float derivative = (error - prevError) / dt;            //derivative là đạo hàm , tính sự thay đổi sai số theo thời gian
+    float derivative = (error - prevError) / dt;            //derivative là đạo hàm, tính sự thay đổi sai số theo thời gian
 
     float output = Kp * error + Ki * newIntegral + Kd * derivative;    
-    output = std::clamp(output, -maxOutput, maxOutput);   
+    output = std::clamp(output, -maxOutput, maxOutput);     
     // Chỉ cập nhật output trong tầm motor bù được
     if (output != maxOutput && output != -maxOutput) {
         integral = newIntegral;
     }
 
-    prevError = error;                                   
+    prevError = error;                    //prevError là sai số của lần tính trước               
     return output;                                       
 }
 
@@ -32,4 +32,5 @@ void PID::reset() {
 void PID::setDt(float newDt) {
     dt = newDt;
 }
+
 
