642fe573c18fcc81b6a50bca36abd4de3f653960
nghiamatmat <doan06561@gmail.com>
2025-11-21 17:10:21 +0700

    Update main.cpp

--- EmbedX_Micromouse/include/main.cpp
+++ EmbedX_Micromouse/include/main.cpp
@@ -4,14 +4,14 @@ PID::PID(float p, float i, float d, float deltaTime, float maxI, float maxOut)
     : Kp(p), Ki(i), Kd(d), dt(deltaTime), maxIntegral(maxI), maxOutput(maxOut) {}
 
 // Tính toán giá trị PID
-float PID::tinhtoan(float setpoint, float measured) {
+float PID::tinhtoan(float setpoint, float measured) {        //measured là giá trị đo được trong thực tế , setpoint là giá trị mong muốn
     float error = setpoint - measured;                     
-    float newIntegral = integral + error * dt;            
+    float newIntegral = integral + error * dt;            // Integral là tích phân , để cộng dồn sai số theo thời gian để giảm thiểu sai số về lâu dài 
     newIntegral = std::clamp(newIntegral, -maxIntegral, maxIntegral);
 
-    float derivative = (error - prevError) / dt;
+    float derivative = (error - prevError) / dt;            //derivative là đạo hàm , tính sự thay đổi sai số theo thời gian
 
-    float output = Kp * error + Ki * newIntegral + Kd * derivative; 
+    float output = Kp * error + Ki * newIntegral + Kd * derivative;    
     output = std::clamp(output, -maxOutput, maxOutput);   
     // Chỉ cập nhật output trong tầm motor bù được
     if (output != maxOutput && output != -maxOutput) {
@@ -32,3 +32,4 @@ void PID::reset() {
 void PID::setDt(float newDt) {
     dt = newDt;
 }
+
