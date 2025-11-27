4b852a6494d9475aad2bada0a65a91abb15896aa
dhoangnguyen051107-jpg <dhoangnguyen051107@gmail.com>
2025-11-27 11:42:05 +0700

    Rename motor control variables and functions

--- EmbedX_Micromouse/include/motorControl/motorControl.h
+++ EmbedX_Micromouse/include/motorControl/motorControl.h
@@ -26,22 +26,22 @@ extern const int motorDir2_R;
 // Thông số cơ học
 
 // Khai báo thông số động cơ và tính toán xung di chuyển 
-extern float diameter;// đường kính bánh xe: mét
-extern float distance; // khoảng cách cần di chuyển để đi từ ô này sang ô kia là 0.18 mét
-extern float road_turn; // khoảng cách mỗi bánh phải đi để rẽ đủ 90 độ (số liệu đo đạc thực nghiệm)
-extern float PWM_1cycle; // số xung xuất ra khi bánh quay đủ 1 vòng (số liệu tương đối từ đo đạc thực nghiệm)
+extern float duong_kinh_banh_xe;//mét
+extern float kc ; // khoảng cách cần di chuyển để đi từ ô này sang ô kia là 0.18 mét
+extern float re_trai_phai; // khoảng cách mỗi bánh phải đi để rẽ đủ 90 độ (số liệu đo đạc thực nghiệm)
+extern float so_xung_1_vong_banh_xe ; // số xung xuất ra khi bánh quay đủ 1 vòng (số liệu tương đối từ đo đạc thực nghiệm)
 
-extern float cycles_1square; // số vòng bánh xe để xe tiến được 1 ô
-extern float cycles_turn; // số vòng bánh xe để xe rẽ được 90 độ
+extern float so_vong_di_thang; // số vòng bánh xe để xe tiến được 1 ô
+extern float so_vong_re_trai_phai; // số vòng bánh xe để xe rẽ được 90 độ
 
-extern float PWM_straight; // số xung cần truyền cho encoder biết thời điểm dừng khi đủ khoảng cách mong muốn
-extern float PWM_turn; // xung cần truyền cho encoder biết thời điểm rẽ đủ 90 độ
+extern float xung_can_di_thang; // số xung cần truyền cho encoder biết thời điểm dừng khi đủ khoảng cách mong muốn
+extern float xung_can_re_trai_phai; // xung cần truyền cho encoder biết thời điểm rẽ đủ 90 độ
 
 // Các hàm điều khiển động cơ sẽ được gọi ở thuật toán 
 void setupMotor_control();
-void turnLeft();
-void turnRight();
-void goStraight();
-void turnBack();
-void stop ();
+void re_trai();
+void re_phai();
+void di_thang();
+void quay_dau();
+void dung_lai();
 #endif
