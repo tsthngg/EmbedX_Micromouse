c1244310204609226032950b9d740838b36a9b08
vunhatlong8107-iot <vunhatlong8107@gmail.com>
2025-11-28 12:25:56 +0700

    delete prevFlood, setup floodfill

--- EmbedX_Micromouse/src/main.cpp
+++ EmbedX_Micromouse/src/main.cpp
@@ -6,12 +6,12 @@ Maze maze;
 Robot robot(0, 0, 0);
 
 bool visited[SIZE][SIZE];
-int prevFlood[SIZE][SIZE];
 int curFlood[SIZE][SIZE];
 
 void setup(){
   memset(visited, 0, sizeof(visited));
   visited[0][0] = true;
+  maze.floodFill(curFlood);
 }
 
 void loop(){
